33554c11ab825581e1bb5102c1c9e14b5c13d4fd
Hatzkaichi Hatzka <drohnechan@myzm.local>
2014-10-19 22:46:29 +0900

    dekita

--- js/parallax.js
+++ js/parallax.js
@@ -1,8 +1,11 @@
-$(function(){
-  $(window).scroll(function(){
-    var y = jQuery(this).scrollTop();
-    $('#temae').css('background-position', '0 ' + parseInt( y / 0.3 ) + 'px');
-    $('#naka').css('background-position', '0 ' + parseInt( y / 0.6 ) + 'px');
-    $('#oku').css('background-position', '0 ' + parseInt( y / 0.9 ) + 'px');
+$(function() {
+  var top = $('.pink').offset().top; //y座標の初期値を取得
+  $(window).scroll(function() {
+    var value = $(this).scrollTop();  //スクロール値を取得
+	
+    //スクロール値を除算して移動距離に変化をつける
+    $('.pink').css('top', top + value / 2);
+	$('.blue').css('top', top -700 + value /2);
+	$('.yellow').css('top', top -800 + value /2);
   });
 });
